fc67cd406ce239435debca4b3c273ca6a16b458f
Higor Batista de Souza <higorsouza@unifev.corp.br>
2023-08-21 11:31:40 -0300

    Aula 3 - completa

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -2,25 +2,31 @@
   <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
       <!-- <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg"> -->
-      <video
-        ref="video"
-        class="full-width"
-        autoplay
-      >
+      <video v-show="!imageCaptured" ref="video" class="full-width" autoplay>
       </video>
+
+      <canvas v-show="imageCaptured" ref="canvas" class="full-width" height="240">
+      </canvas>
     </div>
     <div class="text-center q-pa-md">
-      <q-btn round color="grey-10" icon="eva-camera" size="lg" />
+      <!-- if -->
+      <q-btn v-if="hasCameraSupport" @click="captureImage" round color="grey-10" icon="eva-camera" size="lg" />
+      <!-- else -->
+      <q-file v-else label="Choose an image" outlined v-model="imageUpload" accept="image/*" @input="captureImageFallback">
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
 
       <div class="row justify-center q-ma-md">
         <q-input class="col col-sm-6" v-model="post.caption" label="Caption" dense />
       </div>
 
       <div class="row justify-center q-ma-md">
-        <q-input class="col col-sm-6" v-model="post.location" label="Location" dense>
+        <q-input :loading="locationLoading" class="col col-sm-6" v-model="post.location" label="Location" dense>
 
           <template v-slot:append>
-            <q-btn round dense flat icon="eva-navigation-2-outline" />
+            <q-btn v-if="!locationLoading && locationSupported" @click="getLocation" round dense flat icon="eva-navigation-2-outline" />
           </template>
 
         </q-input>
@@ -36,6 +42,7 @@
 <script>
 import { defineComponent, onMounted } from 'vue';
 import { uid } from 'quasar';
+//require('md-gum-polyfill');
 
 export default defineComponent({
   name: 'PageCamera',
@@ -47,17 +54,132 @@ export default defineComponent({
         location: '',
         photo: null,
         date: Date.now()
-      }
+      },
+      imageCaptured: false,
+      imageUpload: [],
+      hasCameraSupport: true,
+      locationLoading: false
+    }
+  },
+  computed: {
+    locationSupported(){
+      if('geolocation' in navigator)
+        return true;
+      return false;
     }
   },
   methods: {
-    initCamera(){
-      console.log('teste cam');
+    initCamera() {
+      //console.log('teste cam');
       navigator.mediaDevices.getUserMedia({
         video: true
       }).then(stream => {
         this.$refs.video.srcObject = stream;
+      }).catch(error => {
+        this.hasCameraSupport = false;
       });
+    },
+    captureImage() {
+      //console.log('clicado');
+      let video = this.$refs.video;
+      let canvas = this.$refs.canvas;
+      canvas.width = video.getBoundingClientRect().width;
+      canvas.height = video.getBoundingClientRect().height;
+      let context = canvas.getContext('2d');
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.imageCaptured = true;
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+      this.disableCamera();
+    },
+    captureImageFallback(event){
+      let file = event.target.files[0];
+      this.post.photo = file;
+
+      //console.log('ARQUIVO: ' + file);
+      //console.log('ARQUIVO TYPE: ' + file);
+
+      let canvas = this.$refs.canvas;
+      let context = canvas.getContext('2d');
+      var reader = new FileReader();
+      reader.onload = event => {
+        var img = new Image();
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img,0,0);
+          this.imageCaptured = true;
+        }
+        img.src = event.target.result;
+      }
+
+      reader.readAsDataURL(file);
+    },
+    disableCamera() {
+      this.$refs.video.srcObject.getVideoTracks().forEach(track => {
+        track.stop();
+      });
+    },
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(',')[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], { type: mimeString });
+      return blob;
+
+    },
+    getLocation(){
+      console.log('getLocation()');
+
+      this.locationLoading = true;
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log('getLocation() 2: ' + position);
+        this.getCityAndCountry(position);
+      }, err => {
+        this.locationError();
+        console.log('getLocation() 3:' + err);
+      }, {timeout: 7000});
+    },
+    getCityAndCountry(position){
+      console.log('getCityAndCountry()');
+
+      let apiUrl = `https://geocode.xyz/${position.coords.latitude}, ${position.coords.longitude}?json=1`;
+      this.$axios.get(apiUrl).then(result => {
+        console.log('Localização trazida com sucesso');
+        this.locationSuccess(result);
+      }).catch(err => {
+        this.locationError();
+        console.log('Não foi possível trazer a localização');
+      });
+    },
+    locationSuccess(result){
+      this.post.location = result.data.city;
+      if(result.data.country){
+        this.post.location += `, ${result.data.country}`;
+      }
+      this.locationLoading = false;
+    },
+    locationError(){
+      this.$q.dialog({
+        title: 'Error',
+        message: 'Could not find your location.'
+      })
+      this.locationLoading = false;
     }
   },
   /* setup() {
@@ -65,9 +187,9 @@ export default defineComponent({
       this.initCamera();
     });
   } */
-  mounted(){
+  mounted() {
     this.initCamera();
-  }
+  },
 
   /* onMounted(() => {
     this.initCamera();
@@ -75,6 +197,11 @@ export default defineComponent({
   /* onMounted(){
     this.initCamera();
   } */
+  beforeDestroy(){
+    if(this.hasCameraSupport){
+      this.disableCamera();
+    }
+  }
 })
 </script>
 
